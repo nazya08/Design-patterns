2e63d76b5f754b44a2465fd95c76bb73e2ef7224
Nazar <nazarfilonyk@gmail.com>
2024-05-22 13:24:15 +0300

    pattern(adapter)

--- design_patterns/structural_patterns/adapter_pattern.py
+++ design_patterns/structural_patterns/adapter_pattern.py
@@ -28,7 +28,7 @@ class Adapter(Target, Adaptee):
         return f"Adapter: (TRANSLATED) {self.specific_request()[::-1]}"
 
 
-def client_code(target: "Target") -> None:
+def client_code(target: Target) -> None:
     """
     The client code supports all classes that follow the Target interface.
     """
@@ -43,8 +43,10 @@ if __name__ == "__main__":
     print("\n")
 
     adaptee = Adaptee()
-    print("Client: The Adaptee class has a weird interface. "
-          "See, I don't understand it:")
+    print(
+        "Client: The Adaptee class has a weird interface. "
+        "See, I don't understand it:"
+    )
     print(f"Adaptee: {adaptee.specific_request()}", end="\n\n")
 
     print("Client: But I can work with it via the Adapter:")
